e9d9877b8f3c7b4b21484ac3b1ce9175698d1ab7
Ландсберг-Дым Иван <whiterex31@gmail.com>
2023-02-02 14:43:35 +0300

    Caesar 30%

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+def FileReading():
+
+
 def main():
     path_in = "input.txt"
     path_out = "output.txt"
     print(f"Название исходного файла: {path_in}")
     print(f"Название результирующего файла: {path_out}")
-
-
-
 
 
 if __name__ == '__main__':
